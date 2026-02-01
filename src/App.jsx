fda4521648e7eadad4f011fbe8fb002ba982220c
AnuragBhashkar <anuragbhashkarofficial@gmail.com>
2026-02-02 03:33:40 +0530

    feat: add geolocation support using custom React hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import useGeolocation from "./hooks/useGeolocation";
+
 function App() {
+  const { location, error, loading } = useGeolocation();
+
+  if (loading) {
+    return <p>Getting your location...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
   return (
     <div>
       <h1>NearWise</h1>
+      <p>Latitude: {location.lat}</p>
+      <p>Longitude: {location.lng}</p>
     </div>
   );
 }
